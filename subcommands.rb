a03912b91f16496d2458c6472227237bdec68a21
Mark Meves <mark.meves@gmail.com>
2011-01-24 00:35:27 -0500

    total overhaul to subcommand (task) architecture ..
    subcommands (tasks) are anonymous classes, objects of them are created
    to produce documentation and to execute (familiar pattern!)

--- subcommands.rb
+++ subcommands.rb
@@ -1,5 +1,5 @@
 module Hipe::InterfaceReflector
-  module DispatcherModuleMethods
+  module SubcommandModuleMethods
     include ::Hipe::InterfaceReflector::ModuleMethods
     def build_interface
       ::Hipe::InterfaceReflector::RequestParser.new do |o|
@@ -13,30 +13,35 @@ module Hipe::InterfaceReflector
     end
     attr_reader :subcommands
   end
-  module DispatcherCliInstanceMethods
+  module SubcommandCliInstanceMethods
+    # alot of these aren't just for dispatchers but for children
     include ::Hipe::InterfaceReflector::InstanceMethods
     include ::Hipe::InterfaceReflector::CliInstanceMethods
-    def default_action; :dispatch end
     def arguments_syntax_string
-      respond_to?(:subcommands) or
-        return interface_reflector_arguments_syntax_string
-      '[ ' << self.class.subcommands.map(&:name).join(' | ') <<
-        ' ] [opts] [args]'
+      subs = subcommands
+      subs or return interface_reflector_arguments_syntax_string
+      '[ ' << subs.map(&:name).join(' | ') << ' ] [opts] [args]'
     end
     def build_documenting_option_parser
-      if ! self.class.respond_to?(:subcommands)
-        return interface_reflector_build_documenting_option_parser
-      end
-      # overrides 'parent', assumes exactly one argument! see overridden
+      subs = self.class.respond_to?(:subcommands) ?
+        self.class.subcommands : nil
+      subs.nil? and return interface_reflector_build_documenting_option_parser
       ophack = cli_option_parser.dup
       ophack.separator(em("subcommands:"))
       self.class.subcommands.each do |c|
-        hack_add_argument_to_optparse(ophack, c)
+        hack_add_argument_to_optparse(ophack,
+          c.subcommand_documenting_instance)
       end
       ophack
     end
+    def default_action; :dispatch end
+    alias_method :execution_context=, :c=
     def find_subcommand attempt
-      subs = self.class.subcommands
+      subs = subcommands
+      if subs.nil?
+        return [nil, "#{color(subcommand_fully_qualified_name, :green)} "<<
+          "has no subcommands.  Does not respond to #{attempt.inspect}"]
+      end
       found = subs.detect { |s| s.name == attempt }
       if subcommand_soft_match? && ! found
         re = %r{\A#{Regexp.escape(attempt)}}
@@ -55,18 +60,19 @@ module Hipe::InterfaceReflector
       ]
       return [found, nil]
     end
+    attr_accessor :invoked_with
     def on_help arg
+      @options_ok = false # pretend there was a parse failure
+      ['-h', '--help'].include?(arg) and arg = nil
+      if arg && arg[0,1] == '-'
+        @c.err.puts "ignoring: #{arg.inspect}"
+        arg = nil
+      end
       arg.nil? and return interface_reflector_on_help
       found, msg = find_subcommand(arg)
       found or return error(msg)
-      # this is terrible smell that can be fixed later
-      if found.respond_to? :show_help
-        found.show_help self, @argv.dup
-        @exit_ok = true
-        false
-      else
-        found.on_help @argv.first
-      end
+      @exit_ok = true # supress self parse error message!
+      found.subcommand_on_help @argv, self
     end
     def on_parse_failure
       # overrides 'parent', don't surreptitously display help (todo)
@@ -81,13 +87,21 @@ module Hipe::InterfaceReflector
       end
       true # no parsing of opts, deferred
     end
+    def subcommands
+      self.class.respond_to?(:subcommands) ? self.class.subcommands : nil
+    end
+    def subcommand_fully_qualified_name
+      if respond_to? :parent
+        "#{parent.subcommand_fully_qualified_name} #{cli_label}"
+      else
+        program_name
+      end
+    end
     def subcommand_soft_match?; true end
     def parse_args
-      self.class.respond_to?(:subcommands) or
-        return interface_reflector_parse_args
+      (subs = subcommands) or return interface_reflector_parse_args
       @argv.empty? and return error("expecting subcommand: " <<
-        oxford_comma(
-          self.class.subcommands.map{ |c| color(c.name, :green)},' or '))
+        oxford_comma(subs.map{ |c| color(c.name, :green)},' or '))
       attempt = @argv.first
       found, msg = find_subcommand attempt
       found or return error(msg)
@@ -95,45 +109,23 @@ module Hipe::InterfaceReflector
       true
     end
     def dispatch
-      @subcommand.execution_context = @c
-      @subcommand.parent = self
-      @subcommand.invoked_with = @argv.shift
-      @subcommand.run @argv
+      child = @subcommand.subcommand_execution_instance
+      child.execution_context = @c
+      child.parent = self
+      child.invoked_with = @argv.shift
+      child.run @argv
     end
   end
-  module CommandDefinition
-    def self.extended mod
-      mod.class_eval do
-        # for now it's as if we are just including, not extending but this is
-        # preserved in case we add more DSL-like features to the Command class
+  module CommandDefinitionClass
+    def self.extended cls
+      cls.class_eval do
+        extend  CommandDefinitionModuleMethods
         include CommandDefinitionInstanceMethods
       end
     end
-    module CommandDefinitionInstanceMethods
+    module CommandDefinitionModuleMethods
       include Hipe::InterfaceReflector::InstanceMethods
-
-      def cli_label; name end
-      def intern;    name end
-      attr_reader   :name
-      attr_accessor :invoked_with # just in case you need to know
-      def option_parser &block
-        if ! block_given?
-          interface
-        elsif @interface_definition_block
-          fail("implement  merging")
-        else
-          @interface_definition_block = block
-        end
-      end
-      def parent= p
-        class << self ; self end.send(:define_method, :parent) { p }
-      end
-      def show_help parent, argv
-        self.parent = parent
-        @c = parent.execution_context
-        @argv = argv
-        on_help argv.shift
-      end
+      include Hipe::InterfaceReflector::SubcommandModuleMethods
       def interface
         @interface ||= begin
           if @interface_definition_block.nil?
@@ -145,14 +137,33 @@ module Hipe::InterfaceReflector
           end
         end
       end
+      def intern; name end
+      def option_parser &block
+        if ! block_given?
+          interface
+        elsif @interface_definition_block
+          fail("interface merging not supported!")
+        else
+          @interface_definition_block = block
+        end
+      end
+      def subcommand_documenting_instance; new end
+      def subcommand_execution_instance;   new end
+      def subcommand_on_help argv, parent
+        new.subcommand_on_help argv, parent
+      end
     end
-  end
-  module CliInstanceMethods
-    def parent_usage
-      if respond_to?(:parent)
-        "#{parent.parent_usage} #{name.to_s}"
-      else
-        "#{em('usage:')} #{program_name}"
+    module CommandDefinitionInstanceMethods
+      def cli_label; self.class.intern.to_s end
+      def intern;    self.class.intern      end
+      def parent= p
+        class << self ; self end.send(:define_method, :parent) { p }
+      end
+      def subcommand_on_help argv, parent
+        self.parent = parent
+        @c = parent.execution_context
+        @argv = argv
+        on_help argv.shift
       end
     end
   end
