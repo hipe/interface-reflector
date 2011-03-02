17bfbd0c3bd126ed78e81ddab0deb0ba8ff8a894
Mark Meves <mark.meves@gmail.com>
2011-03-01 23:27:09 -0500

    OmniTable: cleanup, fix for when   doing ascii terminal escape sequences
    (this was messing up the widths, both when calculating them
    and when using sprintf for outputting left- and right-aligned
    cels)

--- omni-table.rb
+++ omni-table.rb
@@ -9,9 +9,9 @@
 #
 #   Hipe::InterfaceReflector::OmniTable.new(rows, col_meta).to_ascii($stdout)
 #
-#       Phone column 2
-#      iphone 329.99
-#     android 29.99
+#       Phone  column 2
+#      iphone  329.99
+#     android  29.99
 #
 # Some setters are available to affect the look of the thing.
 # Try: sep(' | ').no_headers! for:
@@ -28,72 +28,69 @@ module Hipe::InterfaceReflector
       @cols = cols.each.with_index.map do |c,i|
         c.respond_to?(:render) ? c : Col.new({:column_index => i}.merge(c))
       end
-      @rows = rows
       @headers = true
+      @rows = rows
+      @sep = '  '
     end
     attr_reader :cols, :rows, :headers
     alias_method :headers?, :headers
     def no_headers!; @headers = false; self end
     def to_ascii out
-      headers? and out.puts(header_row_text)
-      rendered_rows.each do |row|
-        out.puts row_printf_format % row
+      matrix = rendered_matrix
+      headers? and matrix.unshift(@cols.map(&:label))
+      widths
+      matrix.each do |row|
+        out.puts(@cols.map do |c|
+          f = @widths[c.intern] - len(row[c.intern])
+          "#{c.fill * f if c.right?}#{row[c.intern]}#{c.fill * f if c.left?}"
+        end.join(@sep))
       end
       out
     end
-    def header_row_text
-      @header_row_text ||= begin
-        @header_cels = @cols.map(&:label)
-        row_printf_format % @header_cels
-      end
-    end
-    def render_row r
-      @cols.map { |c| c.render(r[c.intern]) }
-    end
-    def rendered_rows
-      instance_variable_defined?('@prerendered') ? @prerendered : prerender
-    end
-    def row_printf_format
-      @row_printf_format ||= begin
-        @widths = instance_variable_defined?('@header_cels') ?
-          Hash[ *
-            @header_cels.each.with_index.map { |s,i| [i, s.length] }.flatten
-          ] : Hash.new { |h, k| h[k] = 0 };
-        rendered_rows.each do |row|
-          row.each_with_index do |v, i|
-            @widths[i] > v.length or @widths[i] = v.length
-          end
-        end
-        (0..@widths.size-1).map do |i|
-          "%#{'-' if :left == @cols[i].align}#{@widths[i]}s"
-        end.join(sep)
-      end
-    end
     def sep *a
-      if a.any?
-        @sep = a.first
-        self
-      else
-        instance_variable_defined?('@sep') ? @sep : ' '
-      end
+      a.empty? and return @sep
+      @sep = a.first
+      self
     end
     class Col
       def initialize h
         @label = h[:label]
         @align = h[:align] || :right
+        @fill  = h[:fill]  || ' '
         if h[:column_index]
           @intern = h[:column_index]
           @label.nil? and @label = "column #{h[:column_index]+1}"
         end
       end
-      attr_reader :label, :align, :intern
+      attr_reader :align, :fill, :intern, :label
+      def left?;  @align == :left  end
+      def right?; @align == :right end
       def render v
         v.to_s
       end
     end
   protected
-    def prerender
-      @prerendered = @rows.map { |r| render_row(r) }
+    def len mixed
+      mixed.index("\e") or return mixed.length;
+      mixed.gsub(/\e[^m]+m/, '').length
+    end
+    def rendered_matrix
+      @rendered_matrix ||=
+        @rows.map { |r| @cols.map { |c| c.render(r[c.intern]) } }
+    end
+    def widths
+      @widths ||= begin
+        w = instance_variable_defined?('@header_cels') ?
+          Hash[ *
+            @header_cels.each.with_index.map { |s,i| [i, len(s)] }.flatten
+          ] : Hash.new { |h, k| h[k] = 0 };
+        rendered_matrix.each do |row|
+          row.each_with_index do |v, i|
+            w[i] > len(v) or w[i] = len(v)
+          end
+        end
+        w
+      end
     end
   end
 end
